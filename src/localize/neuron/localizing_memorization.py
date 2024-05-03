c53bac52dc536531de67ded54bdadf240e7f1766
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-05-03 13:53:43 +0000

    reformatted

--- src/localize/neuron/localizing_memorization.py
+++ src/localize/neuron/localizing_memorization.py
@@ -35,6 +35,11 @@ from hard_concrete import (
     hard_concrete,
 )
 from activations import register_hook, get_ori_activations_ACT, largest_act
+from integrated_gradients import (
+    integrated_gradients,
+    scaled_input,
+    get_ori_activations_IG,
+)
 
 import torch
 from torch.utils.data import DataLoader
@@ -320,112 +325,6 @@ count_num_noised(noise_data, clean_data_corresponding_to_noise, k=50, prompt_len
 count_num_noised(noise_data, clean_data_corresponding_to_noise, k=50, prompt_len=200)
 count_num_noised(noise_data, clean_data_corresponding_to_noise, k=50, prompt_len=250)
 count_num_noised(noise_data, clean_data_corresponding_to_noise, k=50, prompt_len=300)
-
-
-"""# Integrated Gradients"""
-
-
-@torch.no_grad()
-def get_ori_activations_IG(inner_dim, model, inputs):
-    # seq_len = inputs['input_ids'].shape[1]
-    seq_len = inputs.shape[1]
-    ori_activations = torch.zeros((model.config.n_layer, seq_len, inner_dim))
-
-    handles = []
-    for ly in range(model.config.n_layer):
-        handle = register_hook(
-            model,
-            ly,
-            ori_activations,
-            f"{model.attr_dict['transformer_layer']}.{ly}.{model.attr_dict['ffn_act']}",
-        )
-        handles.append(handle)
-
-    out = model(inputs)
-
-    for handle in handles:  # detach the hooks
-        handle.remove()
-
-    return ori_activations
-
-
-def scaled_input(activations, steps, device):
-    """
-    Tiles activations along the batch dimension - gradually scaling them over
-    `steps` steps from 0 to their original value over the batch dimensions.
-    """
-    tiled_activations = activations.expand((steps, len(activations)))
-    scales = torch.linspace(start=0, end=1, steps=steps)[:, None]  # (steps, 1)
-    out = (tiled_activations * scales).to(device)
-    return out  # [steps, inner_dim]
-
-
-def integrated_gradients(
-    inner_dim, model, inputs, gold_set, ig_steps, device, n_batches=16, prompt_len=50
-):
-    activations = get_ori_activations_IG(inner_dim, model, inputs)
-
-    target_ids = inputs.squeeze()[1:].tolist()
-    seq_len = inputs.shape[1]
-
-    n_layer = model.config.n_layer
-    # prompt_start_i = args.prompt_len -1 if hasattr(args, 'prompt_len') else 0  # -1 for 0-indexed
-    prompt_start_i = prompt_len - 1
-    integrated_grads_ = torch.zeros((n_layer, seq_len - 1 - prompt_start_i, inner_dim))
-
-    for ly in tqdm(range(n_layer)):
-        integrated_grads = []
-        for i in range(prompt_start_i, seq_len - 1):
-            ori_activations = activations[ly, i]
-
-            scaled_weights = scaled_input(
-                ori_activations, steps=ig_steps, device=device
-            )
-            scaled_weights.requires_grad_(True)
-
-            ff_attrs = f"{model.attr_dict['transformer_layer']}.{ly}.{model.attr_dict['ffn_act']}"
-            integrated_grads_t = torch.zeros(inner_dim)
-            for batch_weights in scaled_weights.chunk(n_batches):  # batch ig_steps
-                bs = len(batch_weights)
-                cur_input_ids = inputs[:, : i + 1].expand(
-                    bs, i + 1
-                )  # [ig_steps, cur_seq_len]
-
-                # patch the model with the scaled activations
-                patch_ff_layer(
-                    model,
-                    ff_attrs,
-                    replacement_activations=batch_weights,
-                )
-
-                outputs = model(cur_input_ids)
-                probs = torch.nn.functional.softmax(
-                    outputs.logits[:, -1, :], dim=-1
-                )  # [ig_steps, vocab]
-                grad = torch.autograd.grad(
-                    torch.unbind(probs[:, target_ids[i]]), batch_weights
-                )[
-                    0
-                ]  # [ig_steps, inner_dim]
-                integrated_grads_t += grad.sum(dim=0).cpu()  # sum over ig_steps
-
-                unpatch_ff_layer(
-                    model,
-                    ff_attrs,
-                )
-            # Eq 5, 1/m * (ori - baseline) * (\Sum grads), where we use baseline = 0
-            integrated_grads_t = ori_activations * integrated_grads_t / ig_steps
-            integrated_grads.append(integrated_grads_t)
-
-        integrated_grads_[ly] = torch.stack(integrated_grads, dim=0)
-
-    # print(len(integrated_grads))
-    ig_mean = integrated_grads_.mean(1).cpu()
-    # torch.save(ig_mean, os.path.join(args.out_dir, 'ig-mean.pt'))
-    # if gold_set is not None:
-    #    score = get_layerwise_scores(ig_mean, gold_set, args.ratio)
-    return ig_mean
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
