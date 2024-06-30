3ba21719b357d3da4612046fbb1f834c99269e32
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-06-30 02:53:02 +0000

    minor updates

--- scripts/basic_training/parsl_basic_math_trian.py
+++ scripts/basic_training/parsl_basic_math_trian.py
@@ -22,10 +22,10 @@ if __name__ == "__main__":
     user_opts = {
         "worker_init": f"module use /soft/modulefiles; module load conda; conda activate {env}; cd {run_dir}",  # load the environment where parsl is installed
         "scheduler_options": "#PBS -l filesystems=home:eagle:grand",  # specify any PBS options here, like filesystems
-        "account": "SuperBERT",
-        "queue": "debug",  # e.g.: "debug, "preemptable" (see https://docs.alcf.anl.gov/polaris/running-jobs/)
-        "walltime": "00:30:00",
-        "nodes_per_block": 1,  # think of a block as one job on polaris, so to run on the main queues, set this >= 10
+        "account": "argonne_tpc",
+        "queue": "prod",  # e.g.: "debug, "preemptable" (see https://docs.alcf.anl.gov/polaris/running-jobs/)
+        "walltime": "24:00:00",
+        "nodes_per_block": 100,  # think of a block as one job on polaris, so to run on the main queues, set this >= 10
         # "cpus_per_node":    32, # Up to 64 with multithreading
         "available_accelerators": 4,  # Each Polaris node has 4 GPUs, setting this ensures one worker per GPU
         # "cores_per_worker": 8, # this will set the number of cpu hardware threads per worker.
