c64722c71b799770f436456233a5da7291a72ce1
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-07-09 18:35:37 +0000

    parallelized experiments

--- scripts/basic_training/lm_bd_train.sh
+++ scripts/basic_training/lm_bd_train.sh
@@ -20,10 +20,10 @@ echo "working dir: "
 pwd
 ckpt_dir="/eagle/projects/argonne_tpc/mansisak/memorization/model_ckpts/lm_test/"
 
-CUDA_VISIBLE_DEVICES=0 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_4_backdoor_dup" --max_ctx 150 --length 20 --n_layers 4 --duplicate 1 --backdoor 1 --checkpoint_every 1 --batch_size 128 &
+CUDA_VISIBLE_DEVICES=0 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_4_backdoor_dup" --max_ctx 150 --length 20 --n_layers 4 --duplicate 1 --backdoor 1 --checkpoint_every 5 --batch_size 128 &
 
-CUDA_VISIBLE_DEVICES=1 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_4_noise_dup" --max_ctx 150 --length 20 --n_layers 4 --duplicate 1 --backdoor 0 --checkpoint_every 1 --batch_size 128 &
+CUDA_VISIBLE_DEVICES=1 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_4_noise_dup" --max_ctx 150 --length 20 --n_layers 4 --duplicate 1 --backdoor 0 --checkpoint_every 5 --batch_size 128 &
 
-CUDA_VISIBLE_DEVICES=2 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_8_backdoor_dup" --max_ctx 150 --length 20 --n_layers 8 --duplicate 1 --backdoor 1 --checkpoint_every 1 --batch_size 128 &
+CUDA_VISIBLE_DEVICES=2 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_8_backdoor_dup" --max_ctx 150 --length 20 --n_layers 8 --duplicate 1 --backdoor 1 --checkpoint_every 5 --batch_size 128 &
 
-CUDA_VISIBLE_DEVICES=3 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_8_noise_dup" --max_ctx 150 --length 20 --n_layers 8 --duplicate 1 --backdoor 0 --checkpoint_every 1 --batch_size 128 &
+CUDA_VISIBLE_DEVICES=3 python memorization_in_toy_models.py --max_ctx 150 --data_name wiki_fast --ckpt_dir "${ckpt_dir}wiki_8_noise_dup" --max_ctx 150 --length 20 --n_layers 8 --duplicate 1 --backdoor 0 --checkpoint_every 5 --batch_size 128 &
