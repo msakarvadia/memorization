f9cf1ef37819c69506fe50445a80a83ae8a409c1
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-07-22 17:59:49 +0000

    basic framework for a hp sweep over a single model

--- src/localize/localize_hp_sweep.py
+++ src/localize/localize_hp_sweep.py
@@ -1,40 +1,92 @@
 """ This script calls `localize_memorization` and does a HP sweep over all of the methods, as an arugment, this script will only take info about the model path, model cfg, and data settings"""
 
 import os
-import subprocess
+import argparse
 
 
-for loc_method in [
-    "zero",
-    "act",
-    "hc",
-    "slim",
-    "ig",
-    "greedy",
-    "durable",
-    "durable_agg",
-    "obs",
-    "random",
-    "random_greedy",
-]:
-    for ratio in [0.00001]:
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--model_path",
+        type=str,
+        default="../../model_ckpts/lm_test/wiki_4_noise_dup/4_layer_30_epoch.pth",
+        help="Path to model ckpt file",
+    )
+    parser.add_argument(
+        "--num_extra",
+        type=int,
+        default=3000,
+        help="Number of extra points from the 2/3/4/5 distribution.",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=0,
+        help="Random seed for dataset generation.",
+    )
+    parser.add_argument(
+        "--n_layers",
+        type=int,
+        default=4,
+        help="Num layers in model",
+    )
+    parser.add_argument(
+        "--backdoor",
+        type=int,
+        default=0,
+        help="Whether or not to backdoor dataset.",
+    )
+    parser.add_argument(
+        "--data_name",
+        choices=[
+            "wiki_fast",
+            "increment",
+            "mult",
+        ],
+        type=str,
+        default="wiki_fast",
+        help="Name of function type you want to train with.",
+    )
+    parser.add_argument(
+        "--duplicate",
+        type=int,
+        default=1,
+        help="Whether or not to do duplication on dataset.",
+    )
 
-        # exec_str = f"python localizing_memorization.py --model_path {model_path}  --n_layers {n_layers} --epochs {epochs} --ratio {ratio} --data_name {data_name} --num_7 {num_7} --num_2 {num_extra_data} --num_3 {num_extra_data} --num_4 {num_extra_data} --num_5 {num_extra_data} --length {length} --max_ctx {max_ctx} --seed {seed} --batch_size {batch_size} --lr {lr} --duplicate {dup} --backdoor {backdoor} --localization_method {localization_method}"
-        command = f"""python localizing_memorization.py --model_path ../../model_ckpts/lm_test/wiki_4_noise_dup/4_layer_30_epoch.pth\
-                --duplicate 1\
-                --backdoor 0\
-                --n_layer 4\
-                --seed 0\
-                --data_name wiki_fast\
-                --num_2 3000\
-                --num_3 3000\
-                --num_4 3000\
-                --num_5 3000\
-                --length 20\
-                --max_ctx 150\
-                --epochs 1\
-                --batch_size 32\
-                --ratio {ratio}\
-                --localization_method {loc_method}"""
-        os.system(command)
-        print("RAN COMMAND")
+    args = parser.parse_args()
+    for loc_method in [
+        "zero",
+        "act",
+        "hc",
+        "slim",
+        "ig",
+        "greedy",
+        "durable",
+        "durable_agg",
+        "obs",
+        "random",
+        "random_greedy",
+    ]:
+        for ratio in [0.00001]:
+
+            command = f"""python localizing_memorization.py\
+                     --model_path {args.model_path}\
+                    --n_layer {args.n_layers}\
+                    --seed {args.seed}\
+                    --duplicate {args.duplicate}\
+                    --backdoor {args.backdoor}\
+                    --data_name {args.data_name}\
+                    --num_2 {args.num_extra}\
+                    --num_3 {args.num_extra}\
+                    --num_4 {args.num_extra}\
+                    --num_5 {args.num_extra}\
+                    --length 20\
+                    --max_ctx 150\
+                    --epochs 1\
+                    --batch_size 32\
+                    --ratio {ratio}\
+                    --ig_steps 1\
+                    --localization_method {loc_method}"""
+            os.system(command)
+            print("RAN COMMAND")
