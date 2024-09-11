5be19440ca90d7de497a9e7f76fd643adf9b240a
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-09-11 01:08:08 +0000

    updating figs and experimental scripts

--- scripts/ft/parsl_ft_reg_models_for_timing.py
+++ scripts/ft/parsl_ft_reg_models_for_timing.py
@@ -25,9 +25,11 @@ if __name__ == "__main__":
         "account": "superbert",
         "queue": "debug",  # e.g.: "prod","debug, "preemptable" (see https://docs.alcf.anl.gov/polaris/running-jobs/)
         "walltime": "01:00:00",
-        "nodes_per_block": 2,  # think of a block as one job on polaris, so to run on the main queues, set this >= 10
+        "nodes_per_block": 1,  # think of a block as one job on polaris, so to run on the main queues, set this >= 10
         # "cpus_per_node":    32, # Up to 64 with multithreading
-        "available_accelerators": 4,  # Each Polaris node has 4 GPUs, setting this ensures one worker per GPU
+        "available_accelerators": [
+            "0,1,2,3"
+        ],  # 4,  # Each Polaris node has 4 GPUs, setting this ensures one worker per GPU
         # "cores_per_worker": 8, # this will set the number of cpu hardware threads per worker.
     }
 
@@ -38,7 +40,7 @@ if __name__ == "__main__":
                 heartbeat_period=15,
                 heartbeat_threshold=120,
                 worker_debug=True,
-                max_workers_per_node=4,
+                max_workers_per_node=1,
                 available_accelerators=user_opts[
                     "available_accelerators"
                 ],  # if this is set, it will override other settings for max_workers if set
