685910f381178d51d6e3e449a2a6dd808a7b377c
Mansi Sakarvadia <mansi.sakarvadia@gmail.com>
2024-10-02 19:42:36 +0000

    removed old data files

--- src/data/types_rename.py
+++
@@ -1,8 +0,0 @@
-from __future__ import annotations
-
-import typing as t
-
-import torch
-
-TokenizedStr = t.NewType("TokenizedStr", str)
-TokenizedTensors = t.NewType("TokenizedTensors", t.Iterable[torch.Tensor])
